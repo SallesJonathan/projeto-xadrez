40e42bcfe12ce17cb956b6e18867bc31a5f85eb7
jonathan salles <sallesjonathan01@gmail.com>
2020-07-14 08:46:40 -0300

    inserção do kig e rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,17 @@
 package chess;
 
 import boardGame.Board;
+import boardGame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
+	
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getpieces(){
@@ -19,5 +24,11 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.White), new Position(2, 1) );
+		board.placePiece(new King(board, Color.Black), new Position(0, 4) );
+		board.placePiece(new King(board, Color.White), new Position(7, 4) );
+
+	}
 	
 }
